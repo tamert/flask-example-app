c9949decdcfbe106ffc6d6dae68c68b8455d4bd9
tagaoglu <farerock@gmail.com>
2019-02-17 04:21:11 +0300

    fix

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from pymongo import MongoClient
-from flask import Flask, render_template, request, redirect, make_response, abort
+from flask import Flask, render_template, request, redirect, make_response, abort, url_for, flash, Response
 from bson.objectid import ObjectId
 import hashlib
 import uuid
@@ -9,7 +9,7 @@ pyt
 app = Flask(__name__)
 
 client = MongoClient()
-db = client.ab2019
+db = client.pekdijital
 
 
 def get_user_from_session():
@@ -22,6 +22,7 @@ def get_user_from_session():
         return
 
     user_id = session['user_id']
+
     user = db.users.find_one({
         "_id": user_id,
         "is_admin": True,
@@ -33,18 +34,19 @@ def get_user_from_session():
 @app.route("/remove/<document_id>")
 def remove(document_id):
     if not get_user_from_session():
-        abort(401)
+        return redirect(url_for('login'))
 
     db.messages.remove({
         "_id": ObjectId(document_id)
     })
-    return redirect("/")
+    flash(u'document removed successfully ', 'success')
+    return redirect(url_for('admin'))
 
 
 @app.route("/edit/<document_id>", methods=["GET", "POST"])
 def edit(document_id):
     if not get_user_from_session():
-        abort(401)
+        return redirect(url_for('login'))
 
     if request.method == "POST":
         sender = request.form['sender']
@@ -58,8 +60,8 @@ def edit(document_id):
             }
         }
         )
-
-        return redirect('/')
+        flash(u'Document saved successfully ', 'success')
+        return redirect(url_for('admin'))
 
     message = db.messages.find_one({
         "_id": ObjectId(document_id)
@@ -88,13 +90,21 @@ def admin():
     user = get_user_from_session()
 
     if not user:
-        return "Forbidden."
+        return redirect(url_for('login'))
 
     return render_template(
         "admin.html",
         messages=get_messages(),
         user=user,
     )
+
+
+@app.route("/logout", methods=["GET"])
+def logout():
+    flash(u'You have successfully logged out!', 'success')
+    response = make_response(redirect(url_for('login')))
+    response.set_cookie('session_id', '', expires=0)
+    return response
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -112,7 +122,8 @@ def login():
         })
 
         if not user:
-            return "Wrong email or password."
+            flash(u'Wrong email or password.', 'danger')
+            return redirect(url_for('login'))
 
         session_id = str(uuid.uuid4())
 
@@ -120,13 +131,17 @@ def login():
             "session_id": session_id,
             "user_id": user['_id']
         });
-
-        response = make_response(render_template('login.html', success=True))
+        flash(u'You have successfully logged', 'success')
+        response = make_response(redirect(url_for('admin')))
         response.set_cookie('session_id', session_id)
         return response
+
     return render_template('login.html')
 
 
 if __name__ == '__main__':
+    app.secret_key = 'super secret key'
+    app.config['SESSION_TYPE'] = 'filesystem'
     app.debug = True;
+    app.jinja_env.globals.update(get_user_from_session=get_user_from_session)
     app.run(host="0.0.0.0", port=5000)
